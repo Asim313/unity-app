f8b526ac305d6b493e328562ce9163f891b68392
Asim313 <asimkhurshid313@gmail.com>
2024-11-19 22:18:37 +0500

    updated

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 //@ts-ignore
 import UnityView from '@azesmway/react-native-unity';
-import {View} from 'react-native';
+import {Button, View} from 'react-native';
 
 const App = () => {
+  const [show, setShow] = useState(false);
   return (
-    <View style={{flex: 1}}>
-      <UnityView
-        style={{
-          position: 'absolute',
-          height: '100%',
-          width: '100%',
-          top: 1,
-          bottom: 1,
-        }}
+    <>
+      <View style={{flex: 1}}>
+        {show && (
+          <UnityView
+            style={{
+              position: 'absolute',
+              height: '100%',
+              width: '100%',
+              top: 1,
+              bottom: 1,
+            }}
+          />
+        )}
+      </View>
+      <Button
+        title={show ? 'Close Unity' : 'Launch Unity'}
+        onPress={() => setShow(!show)}
       />
-    </View>
+    </>
   );
 };
 
